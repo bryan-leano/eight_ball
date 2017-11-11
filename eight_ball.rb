433ace3b6a5dbc99ce5f91674fc2c2ef9acca341
Bryan Leanoe <bryan.leano@gmail.com>
2017-11-10 17:04:52 -0700

    finished project

--- eight_ball.rb
+++ eight_ball.rb
@@ -1,1 +1,28 @@
-@answers = ["Follow your heart",]
+require 'pry' 
+require 'colorize'
+
+@answers = ["Follow your heart", 
+            "Don't be a dumbass", 
+            "You do you",
+            "Don't do it",
+            "Ask again",
+            "Try it out!",
+            "Ask your mom",
+            "You need professional help"]
+
+   
+def main
+    puts "Welcome to Magic 8 ball!""\n".green
+    puts "Type a question to get an answer, or type QUIT to end the program.".yellow
+        if 
+            gets.strip.to_s.downcase == "quit"
+            exit
+        else 
+            puts @answers.sample .red 
+            main
+        end
+end
+
+
+
+main
